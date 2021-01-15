8340b96fe8d1fa4b3827b2ae1c3c24d37075dcbe
CKyed <christiankyed@hotmail.com>
2021-01-15 13:40:42 +0100

    Fjernet overflødig kode

--- app/src/main/java/dk/gruppea3moro/moroa3/findevent/ShowResultFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/findevent/ShowResultFragment.java
@@ -163,28 +163,3 @@ public class ShowResultFragment extends Fragment {
         this.savedEvents = savedEvents;
     }
 }
-
-/*
-public class Toaster {
-    private static final int SHORT_TOAST_DURATION = 2000;
-
-    private Toaster() {}
-
-    public static void makeLongToast(String text, long durationInMillis) {
-        final Toast t = Toast.makeText(getConetex, text, Toast.LENGTH_SHORT);
-        t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-
-        new CountDownTimer(Math.max(durationInMillis - SHORT_TOAST_DURATION, 1000), 1000) {
-            @Override
-            public void onFinish() {
-                t.show();
-            }
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-                t.show();
-            }
-        }.start();
-    }
-}
-*/
